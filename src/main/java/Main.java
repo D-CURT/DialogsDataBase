dc2465575ea56091cec1cf277e49b8ccb7fa1bc4
Админ <decurt_gogencolern@mail.ru>
2018-11-26 16:22:31 +0300

    Code corrections;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
         //System.out.println(UserDAO.getUser("wru3e").getId());
         RelationsDAO.askQuestion("Fred", "What is going on with you?");
         RelationsDAO.answerQuestion("Fred", "What is going on with you?", "It`s not your business!");
-        //UserDAO.removeUser("Fred");
+        UserDAO.removeUser("Fred");
         //UserDAO.removeUser("Katrine");
     }
 }
