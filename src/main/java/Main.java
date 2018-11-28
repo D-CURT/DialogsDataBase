54d7318fa577aafa1323fe85d9bc7777d59f6a02
Админ <decurt_gogencolern@mail.ru>
2018-11-28 13:52:59 +0300

    Test of conversion updated;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,23 +6,13 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        /*JDBCUserImpl jdbc = new JDBCUserImpl();
-        System.out.println(jdbc.addUser("Alex"));
-        //System.out.println(jdbc.removeUser("Alex"));
-        //System.out.println(jdbc.removeQuestion("How are you?", "Alex"));
-        //System.out.println(jdbc.getFullData());
-        System.out.println(jdbc.askQuestion("Alex", "How are you?"));
-        System.out.println(jdbc.askQuestion("Alex", "What are you doing?"));
-        System.out.println(jdbc.answerQuestion("What are you doing?", "i`m going home", "Alex"));
-        System.out.println(jdbc.answerQuestion("How are you?", "i`m fine, thanks", "Alex"));*/
-        //System.out.println(SQLSection.ADD_QUESTION.name());
+
         HibernateUserImpl user = new HibernateUserImpl();
         HibernateRelationsImpl relation = new HibernateRelationsImpl();
         //System.out.println(HibernateUserImpl.getUser(3).getQuestions());
-        System.out.println(user.getUser("alex"));
+        System.out.println(user.getUser("john"));
         //System.out.println(HibernateUserImpl.getUser("wru3e").getId());
         relation.askQuestion("john", "What is going on with you?");
-        //relation.answerQuestion("piter", "Waths`up, bro?", "I`m great!");
 ////        user.removeUser("Fred");
         user.addUser("jOHN", "321");
         user.removeUser("fRED");
--- src/test/java/controllers/UserControllerTest.java
+++ src/test/java/controllers/UserControllerTest.java
@@ -5,9 +5,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.hibernate.query.Query;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import utils.connectors.SessionFactoryManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -96,16 +98,29 @@ public class UserControllerTest extends Mockito {
         assertThat(BEFORE, lessThan(AFTER));
     }
 
+    @SuppressWarnings("JpaQlInspection")
     @Test
     public void check_of_column_name_conversion() {
         final String USER_NAME = "tEST";
+        final String NAME_FIELD = "name";
+        final String COLUMN_IN_DB = "select name from users where name = :name";
         testHandler.addUser(USER_NAME, "123");
 
-        String expected = Character.toUpperCase(USER_NAME.charAt(0))
-                          + USER_NAME.substring(1).toLowerCase();
-        String actual = testHandler.getUser(USER_NAME).getName();
+        Query query = SessionFactoryManager.getInstance()
+                                           .getSession()
+                                           .createSQLQuery(COLUMN_IN_DB);
+        query.setParameter(NAME_FIELD, USER_NAME.toLowerCase());
 
-        assertThat(expected, is(equalTo(actual)));
+        final String ACTUAL_IN_DB = (String) query.uniqueResult();
+        final String EXPECTED_IN_DB = USER_NAME.toLowerCase();
+
+        assertThat(EXPECTED_IN_DB, is(equalTo(ACTUAL_IN_DB)));
+
+        final String ACTUAL_OUT_DB = testHandler.getUser(USER_NAME).getName();
+        final String EXPECTED_OUT_DB = Character.toUpperCase(USER_NAME.charAt(0))
+                          + USER_NAME.substring(1).toLowerCase();
+
+        assertThat(EXPECTED_OUT_DB, is(equalTo(ACTUAL_OUT_DB)));
 
         testHandler.removeUser(USER_NAME);
     }
