4b44b2b928ef51f209c57d2747bb48d7bea0b536
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-11-30 10:28:54 +0300

    OnSave method of UserInterceptor class changed;

--- src/main/java/utils/interceptors/UserInterceptor.java
+++ src/main/java/utils/interceptors/UserInterceptor.java
@@ -9,37 +9,28 @@ import java.util.Arrays;
 
 public class UserInterceptor extends EmptyInterceptor {
 
-    /*@Override
-    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
-        if (entity instanceof User) {
-            User user = (User) entity;
-            if (user.getAge() == null) {
-                user.setAge(6);
-                Object[] newState = Arrays.copyOf(currentState, currentState.length);
-                newState[0] = 6;
-                currentState = newState;
-                System.out.println(types);
-                System.out.println(currentState);
-                System.out.println(user);
-                entity = user;
-            }
-        }
-        return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
-    }*/
-
     @Override
     public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
         if (entity instanceof User) {
             User user = (User) entity;
             if (user.getAge() == null) {
-                user.setAge(6);
-                state[3] = 6;
-                propertyNames[0] = "id";
-                propertyNames[3] = "age";
+                for (int i = 0; i < propertyNames.length; i++) {
+                    if (propertyNames[i].equals("age")) {
+                        state[i] = "6";
+                    }
+                }
                 System.out.println(state);
                 System.out.println(user);
-                entity = user;
             }
+            if ( user.getName() == null
+              || user.getPassportKey() == null
+              || user.getName().isEmpty()
+              || user.getPassportKey().isEmpty()
+               )
+            {
+                throw new IllegalArgumentException();
+            }
+            return true;
         }
         return super.onSave(entity, id, state, propertyNames, types);
     }
