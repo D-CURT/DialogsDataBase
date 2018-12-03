5f856813333b4e67851af6f01d99aa81f8f47963
Админ <decurt_gogencolern@mail.ru>
2018-12-03 16:54:36 +0300

    CopyPast fixed;;

--- src/main/java/utils/interceptors/validators/AdministratorInterceptor.java
+++ src/main/java/utils/interceptors/validators/AdministratorInterceptor.java
@@ -12,29 +12,16 @@ public class AdministratorInterceptor extends EmptyInterceptor {
 
     @Override
     public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
-
-        if (entity instanceof Administrator) {
-            Administrator user = (Administrator) entity;
-            if (user.getAge() == null) {
-                for (int i = 0; i < propertyNames.length; i++) {
-                    if (propertyNames[i].equals("age")) {
-                        state[i] = "6";
-                    }
+        if (super.onSave(entity, id, state, propertyNames, types)) {
+            if (entity instanceof Administrator) {
+                Administrator user = (Administrator) entity;
+                if (user.getPassword() == null || user.getPassword().isEmpty())
+                {
+                    throw new IllegalArgumentException("Unexpected values of the user properties");
                 }
+                return true;
             }
-            if
-            (      user.getName() == null
-                    || user.getPassportKey() == null
-                    || user.getPassword() == null
-                    || user.getName().isEmpty()
-                    || user.getPassportKey().isEmpty()
-                    || user.getPassword().isEmpty()
-            )
-            {
-                throw new IllegalArgumentException("Unexpected values of the user properties");
-            }
-            return true;
         }
-        return super.onSave(entity, id, state, propertyNames, types);
+        return false;
     }
 }
--- src/main/java/utils/interceptors/validators/PremiumUserInterceptor.java
+++ src/main/java/utils/interceptors/validators/PremiumUserInterceptor.java
@@ -9,32 +9,20 @@ import utils.annotations.Interceptor;
 import java.io.Serializable;
 
 @Interceptor(interceptedType = PremiumUser.class)
-public class PremiumUserInterceptor extends EmptyInterceptor {
+public class PremiumUserInterceptor extends UserInterceptor {
 
     @Override
     public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
-        if (entity instanceof PremiumUser) {
-            PremiumUser user = (PremiumUser) entity;
-            if (user.getAge() == null) {
-                for (int i = 0; i < propertyNames.length; i++) {
-                    if (propertyNames[i].equals("age")) {
-                        state[i] = "6";
-                    }
+        if (super.onSave(entity, id, state, propertyNames, types)) {
+            if (entity instanceof PremiumUser) {
+                PremiumUser user = (PremiumUser) entity;
+                if (user.getCreditCard() == null || user.getCreditCard().isEmpty())
+                {
+                    throw new IllegalArgumentException("Unexpected values of the user properties");
                 }
+                return true;
             }
-            if
-                (      user.getName() == null
-                    || user.getPassportKey() == null
-                    || user.getCreditCard() == null
-                    || user.getName().isEmpty()
-                    || user.getPassportKey().isEmpty()
-                    || user.getCreditCard().isEmpty()
-                )
-            {
-                throw new IllegalArgumentException("Unexpected values of the user properties");
-            }
-            return true;
         }
-        return super.onSave(entity, id, state, propertyNames, types);
+        return false;
     }
 }
