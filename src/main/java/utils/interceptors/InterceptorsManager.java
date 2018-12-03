422d5ba69fd75d6d0d90d52f2c3340d9804d8268
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-12-03 18:19:54 +0300

    InterceptorCatcher class deleted;

--- src/main/java/utils/interceptors/InterceptorsManager.java
+++ src/main/java/utils/interceptors/InterceptorsManager.java
@@ -2,18 +2,23 @@ package utils.interceptors;
 
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.type.Type;
-import utils.reflection.InterceptorCatcher;
+import org.reflections.Reflections;
+import utils.annotations.Interceptor;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class InterceptorsManager extends EmptyInterceptor {
-    private InterceptorCatcher catcher;
+    private static final Class<Interceptor> TARGET_ANNOTATION = Interceptor.class;
     private String packageName;
+    private Map<String, Set<EmptyInterceptor>> interceptors;
 
     public InterceptorsManager(String packageName) {
         this.packageName = packageName;
-        catcher = new InterceptorCatcher(packageName);
+        findInterceptors();
     }
 
     @Override
@@ -42,7 +47,27 @@ public class InterceptorsManager extends EmptyInterceptor {
     }
 
     private Set<EmptyInterceptor> extractSet(Object entity) {
-         return catcher.getInterceptors()
-                .get(entity.getClass().getName());
+         return interceptors.get(entity.getClass().getName());
+    }
+
+    private void findInterceptors() {
+        interceptors = new HashMap<>();
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(TARGET_ANNOTATION);
+        Set<EmptyInterceptor> interceptorSet;
+        for (Class<?> c: annotated) {
+            String key =
+                    c.getAnnotation(TARGET_ANNOTATION).interceptedType().getName();
+            try {
+                EmptyInterceptor interceptor = (EmptyInterceptor) c.newInstance();
+                interceptorSet =
+                        interceptors.get(key) != null ? interceptors.get(key)
+                                : new HashSet<>();
+                interceptorSet.add(interceptor);
+                interceptors.put(key, interceptorSet);
+            } catch (InstantiationException | IllegalAccessException e) {
+                System.out.println("*** Unknown interceptor ***");
+            }
+        }
     }
 }
