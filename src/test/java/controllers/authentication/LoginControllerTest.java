baf271c50262127c934dfc6fa1c8e7b2e63b8a20
Админ <decurt_gogencolern@mail.ru>
2018-12-10 12:38:23 +0300

    Code corrections in tests;

--- src/test/java/controllers/authentication/LoginControllerTest.java
+++ src/test/java/controllers/authentication/LoginControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import utils.security.SecurityConfig;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -27,19 +28,36 @@ public class LoginControllerTest {
 
     @Test
     public void check_weather_user_login_using_HttpClient() throws IOException {
-        SecurityConfig.Roles role = SecurityConfig.Roles.USER;
-        String roleName = role.name();
-        dbHandler.addUser(TEST_VALUE, TEST_VALUE, TEST_VALUE, TEST_VALUE, null, roleName);
-        String encoding = Base64.getEncoder().encodeToString("Test:Test".getBytes());
 
-        HttpUriRequest httpRequest = new HttpPost("http://localhost:8080/login");
-        httpRequest.setHeader("Authorization", "Basic " + encoding);
-        CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpRequest);
         String expectedStatus = "HTTP/1.1 200 ";
+        CloseableHttpResponse httpResponse = getStatusCodeAfterLogin(TEST_VALUE, TEST_VALUE);
         String actualStatus = httpResponse.getStatusLine().toString();
 
         assertThat(actualStatus, is(equalTo(expectedStatus)));
 
         dbHandler.removeUser(TEST_VALUE);
+    }
+
+    @Test
+    public void check_of_login_negative_result() throws IOException {
+        String suffix = "111";
+        String expectedStatus = "HTTP/1.1 401 ";
+        CloseableHttpResponse httpResponse = getStatusCodeAfterLogin(TEST_VALUE + suffix, TEST_VALUE + suffix);
+        String actualStatus = httpResponse.getStatusLine().toString();
+
+        assertThat(actualStatus, is(equalTo(expectedStatus)));
+
+        dbHandler.removeUser(TEST_VALUE);
+    }
+
+    private CloseableHttpResponse getStatusCodeAfterLogin(String login, String password) throws IOException {
+        SecurityConfig.Roles role = SecurityConfig.Roles.USER;
+        String roleName = role.name();
+        dbHandler.addUser(TEST_VALUE, TEST_VALUE, TEST_VALUE, TEST_VALUE, null, roleName);
+        String encoding = Base64.getEncoder().encodeToString((login + ":" + password).getBytes());
+
+        HttpUriRequest httpRequest = new HttpPost("http://localhost:8080/login");
+        httpRequest.setHeader("Authorization", "Basic " + encoding);
+        return HttpClientBuilder.create().build().execute(httpRequest);
     }
 }
